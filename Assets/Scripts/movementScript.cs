b24c327325efae11cbd1adbf2940fa362ce8b060
Kim <jdfairbanks@alaska.edu>
2020-03-31 19:03:20 -0800

    adding project thus far

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] public float movementSpeed = 3.0f;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPos = transform.position;
+        Vector2 moveVec = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        playerRB.MovePosition(currentPos + moveVec * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/playerSFXscript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSFXscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource walkSFX;
+    private Rigidbody2D playerRB;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        if(GetComponent<AudioSource>()) {
+            walkSFX = GetComponent<AudioSource>();
+            walkSFX.loop = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+            if(walkSFX.isPlaying == false) {
+                walkSFX.Play();
+            }
+        } else {
+            walkSFX.Stop();
+        }
+    }
+}
